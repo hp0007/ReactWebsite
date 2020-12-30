2ddbba0fa48fdd93f7ea803b210e0d210847dc76
hp0007 <harshitgajjar95336@gmail.com>
2020-12-31 02:50:13 +0530

    animation added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,23 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Sidebar from './Sidebar'
 import Button from './ReusableComoponents/Button'
 import Fade from 'react-reveal/Fade'
+import $ from 'jquery'
 
 const Home = () => {
-    
+    $(document).ready(function(){
+        var mouseX, mouseY;
+        var ww = $( window ).width();
+        var wh = $( window ).height();
+        var traX, traY;
+        $(document).mousemove(function(e){
+          mouseX = e.pageX;
+          mouseY = e.pageY;
+          traX = ((4 * mouseX) / 570) + 40;
+          traY = ((4 * mouseY) / 570) + 50;
+          console.log(traX);
+          $(".title").css({"background-position": traX + "%" + traY + "%"});
+        });
+      });
     return (
         <>            
             <div className="container-home" id="home">                
@@ -14,7 +28,7 @@ const Home = () => {
                     <div className="overlay">
                         <Fade right>
                             <div className="content">
-                                <h1>I AM DESIGNER</h1>
+                                <div class="title">I AM DESIGNER</div>  
                                 <p className="seco">I Make Wonderful Things</p>
                                 <p className="thir">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit odit ex reprehenderit eos architecto sed debitis nam optio repellat quis. Aliquid suscipit nesciunt corrupti non quaerat obcaecati unde adipisci et!</p>
                                 <Button name="My Porfolio" link="/portfolio"/>
